a2350e69276cd3e711cf8b7427341e0255fcea05
Ilyeo <jrag.tkd@gmail.com>
2017-07-29 23:01:18 -0500

    Add factory of users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before { @user = FactoryGirl.build(:user) }
+
+  subject { @user }
+
+  describe "when email is not present" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
+
+  it { should respond_to(:first_name) }
+  it { should respond_to(:last_name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:mobile_no) }
+
   it { should validate_presence_of(:first_name) }
   it { should validate_presence_of(:last_name) }
   it { should validate_presence_of(:email) }
