35caea978ae4a6fa87fc91494da379be8e3b6823
Ilyeo <jrag.tkd@gmail.com>
2017-08-22 11:13:27 -0500

    Add tests for request events by id

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,14 +1,24 @@
 class Api::EventsController < ApplicationController
   before_action :set_address
+  before_action :set_address_event, only: [:show]
 
   # GET /api/users/:user_id/addresses/:address_id/events
   def index
     json_response(@address.events)
   end
 
+  # GET /api/users/:user_id/addresses/:address_id/events/:id
+  def show
+    json_response(@event)
+  end
+
   private
 
   def set_address
     @address = Address.find(params[:address_id])
+  end
+
+  def set_address_event
+    @event = @address.events.find_by!(id: params[:id]) if @address
   end
 end
--- spec/requests/events_controller_spec.rb
+++ spec/requests/events_controller_spec.rb
@@ -24,7 +24,9 @@ RSpec.describe 'Events API', type: :request do
     end
 
     context 'when address does not exists' do
-      let(:address_id) { 0 }
+      let(:address_id
+
+      ) { 0 }
 
       it 'returns status code 404' do
         expect(response).to have_http_status(404)
@@ -36,4 +38,30 @@ RSpec.describe 'Events API', type: :request do
     end
   end
 
+  # Test suite for GET /api/users/:user_id/addresses/:address_id/events/:id'
+  describe 'GET /api/users/:user_id/addresses/:address_id/events/:id' do
+    before { get "/api/users/#{user_id}/addresses/#{address_id}/events/#{event_id}" }
+
+    context 'when address event exists' do
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns the event' do
+        expect(json['id']).to eq(event_id)
+      end
+    end
+
+    context 'when user address does not exist' do
+      let(:event_id) { 0 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Event/)
+      end
+    end
+  end
 end
