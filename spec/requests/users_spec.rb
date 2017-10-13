88065d33cfa264ab131a6d0e9ea2e671bbb27c2c
Ilyeo <jrag.tkd@gmail.com>
2017-10-13 17:22:45 -0500

    Describe real class name user controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe 'Users API', type: :request do
+RSpec.describe Api::UsersController, type: :controller do
   let!(:users) { create_list(:user, 10) }
   let(:user_id) { users.first.id }
 
   # Test suite for GET /api/users
   describe 'GET /api/users' do
     # make HTTP get request before each example
-    before { get '/api/users' }
+    before { get :index }
 
     it 'returns users' do
       # Note `json` is a custom helper to parse JSON responses
-      expect(json).not_to be_empty
-      expect(json.size).to eq(10)
+      expect(JSON.parse(response.body)).not_to be_empty
+      expect(JSON.parse(response.body).count).to eq(10)
     end
 
     it 'returns status code 200' do
@@ -22,12 +22,12 @@ RSpec.describe 'Users API', type: :request do
 
   # Test suite for GET /api/users/:id
   describe 'GET /api/users/:id' do
-    before { get "/api/users/#{user_id}" }
+    before { get :show, params: { id: users.first.id } }
 
     context 'when the record exists' do
       it 'returns the todo' do
-        expect(json).not_to be_empty
-        expect(json['id']).to eq(user_id)
+        expect(JSON.parse(response.body)).not_to be_empty
+        expect(JSON.parse(response.body)['id']).to eq(user_id)
       end
 
       it 'returns status code 200' do
@@ -36,7 +36,7 @@ RSpec.describe 'Users API', type: :request do
     end
 
     context 'when the record does not exist' do
-      let(:user_id) { 100 }
+      before { get :show, params: { id: 100 } }
 
       it 'returns status code 404' do
         expect(response).to have_http_status(404)
@@ -50,14 +50,12 @@ RSpec.describe 'Users API', type: :request do
 
   # Test suite for POST /api/users
   describe 'POST /api/users' do
-    # valid payload
-    let(:valid_attributes) { { first_name: 'Rogelio', last_name: 'Alatorre', email: 'roger@apimon.com', mobile_no: '3122111436' } }
 
     context 'when the request is valid' do
-      before { post '/api/users', params: valid_attributes }
+      before { post :create, params: { first_name: 'Rogelio', last_name: 'Alatorre', email: 'roger@apimon.com', mobile_no: '3122111436' } }
 
       it 'creates a user' do
-        expect(json['first_name']).to eq('Rogelio')
+        expect(JSON.parse(response.body)['first_name']).to eq('Rogelio')
       end
 
       it 'returns status code 201' do
@@ -66,25 +64,23 @@ RSpec.describe 'Users API', type: :request do
     end
 
     context 'when the request is invalid' do
-      before { post '/api/users', params: { first_name: 'Israel', last_name: 'Alatorre', mobile_no: '4499607974' } }
+      before { post :create, params: { first_name: 'Israel', last_name: 'Alatorre', email: '', mobile_no: '4499607974' } }
 
       it 'returns status code 422' do
         expect(response).to have_http_status(422)
       end
 
       it 'returns a validation failure message' do
-        expect(response.body)
-          .to match(/Validation failed: Email can't be blank/)
+        expect(response.body).to match(/Validation failed: Email can't be blank/)
       end
     end
   end
 
   # Test suite for PUT /api/users/:id
   describe 'PUT /api/users/:id' do
-    let(:valid_attributes) { { first_name: 'Jose' } }
 
     context 'when the record exists' do
-      before { put "/api/users/#{user_id}", params: valid_attributes }
+      before { put :update, params: { id: users.first.id, first_name: 'Jose' } }
 
       it 'updates the record' do
         expect(response.body).to be_empty
@@ -98,7 +94,7 @@ RSpec.describe 'Users API', type: :request do
 
   # Test suite for DELETE /api/users/:id
   describe 'DELETE /api/users/:id' do
-    before { delete "/api/users/#{user_id}" }
+    before { delete :destroy, params: { id: users.first.id } }
 
     it 'returns status code 204' do
       expect(response).to have_http_status(204)
