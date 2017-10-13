6a7441a364746fc1f2cefb13b83182af50d0fd6d
Ilyeo <jrag.tkd@gmail.com>
2017-10-13 17:01:20 -0500

    Describe real class name event controller

--- spec/requests/events_controller_spec.rb
+++ spec/requests/events_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Events API', type: :request do
+RSpec.describe Api::EventsController, type: :controller do
   let!(:user) { create(:user) }
   let!(:address) { create(:address, user_id: user.id) }
   let!(:events) { create_list(:event, 20, address_id: address.id) }
@@ -11,7 +11,7 @@ RSpec.describe 'Events API', type: :request do
   # Test suite for GET /api/users/:user_id/addresses/:address_id/events
   describe 'GET /api/users/:user_id/addresses/:address_id/events' do
     # make HTTP get request before each example
-    before { get "/api/users/#{user_id}/addresses/#{address_id}/events" }
+    before { get :index, params: { user_id: user.id, address_id: address.id, id: events.first.id } }
 
     context 'when address exists' do
       it 'returns status code 200' do
@@ -19,14 +19,12 @@ RSpec.describe 'Events API', type: :request do
       end
 
       it 'returns all address events' do
-        expect(json.size).to eq(20)
+        expect(JSON.parse(response.body).count).to eq(20)
       end
     end
 
     context 'when address does not exists' do
-      let(:address_id
-
-      ) { 0 }
+      before { get :index, params: { user_id: user.id, address_id: 0, id: events.first.id } }
 
       it 'returns status code 404' do
         expect(response).to have_http_status(404)
@@ -40,7 +38,7 @@ RSpec.describe 'Events API', type: :request do
 
   # Test suite for GET /api/users/:user_id/addresses/:address_id/events/:id'
   describe 'GET /api/users/:user_id/addresses/:address_id/events/:id' do
-    before { get "/api/users/#{user_id}/addresses/#{address_id}/events/#{event_id}" }
+    before { get :show, params: { user_id: user.id, address_id: address.id, id: events.first.id } }
 
     context 'when address event exists' do
       it 'returns status code 200' do
@@ -48,12 +46,12 @@ RSpec.describe 'Events API', type: :request do
       end
 
       it 'returns the event' do
-        expect(json['id']).to eq(event_id)
+        expect(JSON.parse(response.body)['id']).to eq(events.first.id)
       end
     end
 
     context 'when user address does not exist' do
-      let(:event_id) { 0 }
+      before { get :show, params: { user_id: user.id, address_id: address.id, id: 0 } }
 
       it 'returns status code 404' do
         expect(response).to have_http_status(404)
@@ -67,10 +65,9 @@ RSpec.describe 'Events API', type: :request do
 
   # Test suite for GET /api/users/:user_id/addresses/:address_id/events'
   describe 'POST /api/users/:users_id/addresses/:address_id/events' do
-    let(:valid_attributes) { { zone_code: 07, zone_description: 'Main door', event_type: 'Fuego'  } }
 
     context 'when request attributes are valid' do
-      before { post "/api/users/#{user_id}/addresses/#{address_id}/events", params: valid_attributes }
+      before { post :create, params: { user_id: user.id, address_id: address.id, zone_code: 07, zone_description: 'Main door', event_type: 'Fuego' } }
 
       it 'returns status code 201' do
         expect(response).to have_http_status(201)
@@ -78,7 +75,7 @@ RSpec.describe 'Events API', type: :request do
     end
 
     context 'when an invalid request' do
-      before { post "/api/users/#{user_id}/addresses/#{address_id}/events", params: {} }
+      before { post :create, params: { user_id: user.id, address_id: address.id } }
 
       it 'returns status code 422' do
         expect(response).to have_http_status(422)
@@ -92,9 +89,7 @@ RSpec.describe 'Events API', type: :request do
 
   # Test suite for PUT /api/users/:user_id/addresses/:address_id/events/:id
   describe '/api/users/:user_id/addresses/:address_id/events/:id' do
-    let(:valid_attributes) { { zone_description: 'Living room' } }
-
-    before { put "/api/users/#{user_id}/addresses/#{address_id}/events/#{event_id}", params: valid_attributes }
+    before { put :update, params: { user_id: user.id, address_id: address.id, id: events.first.id, zone_description: 'Living room' } }
 
     context 'when event exist' do
       it 'returns status code 204' do
@@ -108,7 +103,7 @@ RSpec.describe 'Events API', type: :request do
     end
 
     context 'when the event does not exist' do
-      let(:event_id) { 0 }
+      before { put :update, params: { user_id: user.id, address_id: address.id, id: 0, zone_description: 'Living room' } }
 
       it 'returns status code 404' do
         expect(response).to have_http_status(404)
@@ -122,7 +117,7 @@ RSpec.describe 'Events API', type: :request do
 
   # Test suite for DELETE /api/users/:user_id/addresses/:address_id/events/:id
   describe 'DELETE /api/users/:user_id/addresses/:address_id/events/:id' do
-    before { delete "/api/users/#{user_id}/addresses/#{address_id}/events/#{event_id}" }
+    before { delete :destroy, params: { user_id: user.id, address_id: address.id, id: events.first.id } }
 
     it 'returns status code 204' do
       expect(response).to have_http_status(204)
